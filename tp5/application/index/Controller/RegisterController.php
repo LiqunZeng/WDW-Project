e86d4b507a9e942d30a7c1e33502d7d52b6a32df
LiqunZeng <LiqunZeng@users.noreply.github.com>
2018-04-10 16:08:16 +1000

    注册

--- tp5/application/index/Controller/RegisterController.php
+++ tp5/application/index/Controller/RegisterController.php
@@ -1,23 +1,55 @@
 <?php
 namespace index\Controller;
 use Think\Controller;
+
 class RegisterController extends Controller {
     //显示注册页面
     public function reg(){
         $this->display();
     }
 
-    //检查用户是否注册过
-    public function checkName(){
+    //检查用户的validation
+    public function checkUser(){
+		$error = false;
+		
         $username=$_GET['username'];
         $user=M('User');
-        $where['username']=$username;
-        $count=$user->where($where)->count();
+        $userInfor['username']=$username;
+        $count=$user->where($username)->count();
         if($count){
-            echo '不允许';
-        }else{
-            echo '允许';
+			$error = true;
+			$nameCheck = "username exists.";
+            echo 'username has been used.';
         }
+		else if(empty($username)){
+			$error = true;
+			$usernameCheck = "username cannot be empty."
+			echo 'username cannot be empty';
+		}
+		else if(!preg_match("/^[a-z\d]*$/i",$username)){
+			$error = true;
+			$usernameCheck = "username can only be letters and numbers."
+			echo 'invalid username.'
+		}
+		else{
+			$userPassword = md5(I('password'));
+			if(empty($userPassword)){
+				$error = true;
+				$passwordCheck = "password cannot be empty."
+				echo 'please enter your password.'
+			}else if (strlen($userPassword)<6){
+				$error = true;
+				$passwordCheck = "password too short."
+				echo 'password length must greater or equal than 6'
+			}
+			
+			$userEmail = I('email');	
+			if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+				$error = true;
+				$emailCheck = "Please enter valid email address.";
+            
+        }
+				
     }
     //注册
     public function doReg(){
@@ -31,7 +63,7 @@ class RegisterController extends Controller {
         if($lastId){
             $this->redirect('Index/index');
         }else{
-            $this->error('用户注册失败');
+            $this->error('fail register');
         }
 
     }
