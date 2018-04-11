4b8663d3d11637589d494f1366d3b2bf1babcb89
LiqunZeng <LiqunZeng@users.noreply.github.com>
2018-04-11 15:04:34 +1000

    login

--- tp5/application/index/Controller/LoginController.php
+++ tp5/application/index/Controller/LoginController.php
@@ -14,20 +14,33 @@ class LoginController extends Controller {
         //判断用户在数据库中是否存在
         //存在 允许登录
         //不存在 显示错误信息
-        $username=$_POST['username'];
-        $password=$_POST['password'];
-
-        $user=M('User');
-        $where['username']=$username;
-        $where['password']=$password;
-        $arr=$user->field('id')->where($where)->find();
-        if($arr){
+		
+		$username=$_POST['username'];//get username
+        $password=$_POST['password'];//get password
+		
+		//username and password cannot be empty
+		if(!empty($username)&&!empty($password)){	
+			$user=M('User');//实例化模型
+			$where['username']=$username;
+			$where['password']=$password;
+			
+			// 执行查询
+			$arr=$user->field('id')->where($where)->find();
+			//$user->query("select *from users where uname='$username' and pwd='$password'"); 
+		}
+		
+        if($arr){//如果该用户存在
+			session_start();
+			//将用户名和密码保存在session中
             $_SESSION['username']=$username;
-            $_SESSION['id']=$arr['id'];
-            $this->success('Success',U('Index/index'));
-        
+            $_SESSION['password']=$password;
+			
+			//将用户名和密码保存在cookie中
 			setcookie($username, $password, time()+3600*24)
 		
+            $this->success('Success',U('Index/index'));
+        
+			
 		}else{
             $this->error('incorrect username or password, please check.');
         }
