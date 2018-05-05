8ffc678c26410fbc98c02a800417dbf9b3257ef0
LiqunZeng <LiqunZeng@users.noreply.github.com>
2018-05-05 17:08:12 +1000

    Login完成

---
+++ infs7202/application/Model/Login.php
@@ -0,0 +1,45 @@
+<?php
+
+$name=$_POST[username];
+echo $name;
+$password=$_POST[password];
+$passwordJson = json_encode($password);
+
+$dbms='mysql';
+$host='localhost';
+$dbName='infs3202';
+$user='root';
+$pass='whfywtsdnh';
+$dsn="$dbms:host=$host;dbname=$dbName";
+$dbh = new PDO($dsn, $user, $pass);
+
+session_start();
+$st = $dbh->prepare("SELECT * FROM `user_info` WHERE `username`='$name'");
+$st->execute();
+$user = $st->fetchAll();
+$userJson = json_encode($user);
+  ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script>
+      var user = <?php
+          echo $userJson;
+      ?>;
+      var password = <?php echo $passwordJson ?>;
+      console.log(password);
+      if(user[0][1] === password){
+        alert("success!");
+      }else{
+        alert("fail!");
+        location.href='../View/login.html';
+
+      }
+    </script>
+  </body>
+</html>
