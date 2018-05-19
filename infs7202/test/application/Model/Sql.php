b2af50c290b0b2f4052686c8225ee10697245c83
LiqunZeng <LiqunZeng@users.noreply.github.com>
2018-05-19 18:59:12 +1000

    添加修改了一些东西
    增加了一个connect function，里面用了try catch 作为error handle。

--- infs7202/test/application/Model/Sql.php
+++ infs7202/test/application/Model/Sql.php
@@ -4,11 +4,34 @@ require_once "config/config.php";
 class Sql{
 
     public $connect;
+	
+	////Liqun
+	protected $_dbHandle;
+	protected $_result;
+	////////////
 
     function __construct(){
 
         $this->connect = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
     }
+	
+	///////////////////////////////////Liqun
+	//define('DB_HOST','localhost');
+	//define('DB_USER','root');
+	//define('DB_PASSWORD','');
+	//define('DB_NAME','infs3202');
+	public function connect(){
+	
+		try{
+			$dsn = sprintf("mysql:host=%s;dbname=%s;charset=utf8", DB_HOST, $DB_NAME);
+			$option = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+			$this->_dbHandle = new PDO($dsn, DB_USER, DB_PASSWORD, $option);
+		}catch(PDOException $e){
+			exit('error: '.$e->getMessage());
+		}
+	}
+	//////////////////////////////////////
+		
 
     public function selectAll($table){
 
@@ -44,31 +67,54 @@ class Sql{
         return $row;
     }
 
-	public function registerUser($username,$password,$email,$phone,$gender){
-		$sql = "INSERT INTO user_info (username, password, email, phone, gender) VALUES ($username, $password, $email, $phone, $gender)";
-		$result = $this->connect->query($sql);
-
-        return 'sucess';
+	
+	///////////////////////////////Liqun
+	
+	public function search($table, $column, $id){
+		$sql = sprintf("SELECT * FROM `%t` WHERE %c = '%id'", $table, $column, $username);
+		$sth=$this->_dbHandle->prepare($sql);
+		$sth->execute();
+		return $sth->fetchAll();
 	}
 	
-	public function check($table,$one,$name){
-		$sql = "SELECT * FROM $table WHERE $one ='$name'";
-		$result = $this->connect->query($sql);
-		$check = $result->fetchAll();
-		//$userJson = json_encode($check);
-		//$checkExits = strlen($userJson);
-		return $check;
-		
+	public function add($table, $tablecolumn, $data){
+		$sql = sprintf("INSERT INTO `%t` (%tc) VALUE (%dt) ", $table, $tablecolumn, $data);
+		$sth=$this->_dbHandle->prepare($sql);
+		$sth->execute();
+        return 'success';
 	}
-    public function checkPassword($table,$one,$key, $value){
+	
+	public function checkPassword($table, $username, $password){
+		$sql = sprintf("SELECT * FROM `%t` WHERE username = '%username' AND password = '%password'", $table, $username, $password);
+		$sth=$this->_dbHandle->prepare($sql);
+		$sth->execute();
+		return $sth->fetchAll();
+	}
+	///////////////////////////////
+	
+	///////////////////////////////Liqun
+//	public function check($table,$one,$name){
+//		$sql = "SELECT * FROM $table WHERE $one ='$name'";
+//		$result = $this->connect->query($sql);
+//		$check = $result->fetchAll();
+//		//$userJson = json_encode($check);
+//		//$checkExits = strlen($userJson);
+//		return $check;
+		
+//	}
+//    public function checkPassword($table,$one,$key, $value){
 
-        $sql = "SELECT $one FROM $table WHERE $key = '$value'";
-        $result = $this->connect->query($sql);
-        $i= 0;
-        while ($row[$i] = $result->fetch_array(MYSQLI_ASSOC)){
-            $i++;
-        }
-        return $row;
+//        $sql = "SELECT $one FROM `$table` WHERE $key = $value";
+ //       $result = $this->connect->query($sql);
+ //       $i= 0;
+ //       while ($row[$i] = $result->fetch_array(MYSQLI_ASSOC)){
+ //           $i++;
+ //       }
+  //      return $row;
+//	}
+	////////////////////////////////////
+	
+	
     //unfinished .  add what sql query your want..
 
 
