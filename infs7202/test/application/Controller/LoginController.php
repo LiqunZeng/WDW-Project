04c13126c0ac14165ee03104efd2695f8b028caf
LiqunZeng <LiqunZeng@users.noreply.github.com>
2018-05-19 22:22:37 +1000

    参考YouTube写的

--- infs7202/test/application/Controller/LoginController.php
+++ infs7202/test/application/Controller/LoginController.php
@@ -2,19 +2,19 @@
 require_once 'Controller.php';
 
 require_once 'application/Model/LoginModel.php';
+require_once 'application/Model/UserModel.php';
+
+
 class LoginController extends Controller
-{
+{	
     public function login(){
         $this->display();
 		$this->userLogin();
     }
-	public function userLogin(){
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-		$email = $_POST["email"];
-		$phone = $_POST["phone"];
-		$gender = $_POST["gender"];
-		
+	
+	function userlogin($username, $password){
+
+			//1.check user	
 		$LoginModel = new LoginModel();
 		
 		$checkuser = $LoginModel->search('user_info', 'usernmae', $username);
@@ -23,14 +23,27 @@ class LoginController extends Controller
 		if($checkuser == 0){
 			echo "username does not exits, please check your username or register a new account.";
 		}else{
+
 			$checkpass = $LoginModel->checkPassword('user_info',$username,$password);
 			echo $checkpass;
 			if ($checkpass != null){
+				
+				//if user and password are correct: session_start()
+				session_start();
+				$user = new UserModel($username);
+				$_SESSION['user'] = $user;
+				
+				return true;
 				echo "success!";
 			}else{
+				return false;
 				echo "username or password incorrect, please try again!";
 			}
-		}
+		}		
+	}
+	function userLogout(){
+		session_start();
+		session_destroy();
 	}
 }
 ?>
